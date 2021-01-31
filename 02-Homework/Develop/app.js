fc56f4c9b9c75707f413ec8a2ceab0f9a7451078
Glimpz <abgabbard@gmail.com>
2021-01-31 10:19:18 -0700

    changed question syntax to indicate team member role / name etc

--- 02-Homework/Develop/app.js
+++ 02-Homework/Develop/app.js
@@ -17,7 +17,7 @@ function newExecute() {
       {
         type: "checkbox",
         name: "employeeType",
-        message: "What is your role at the company?",
+        message: "Please select a role for this team member?",
         choices: ["Manager", "Intern", "Engineer"],
       },
     ])
@@ -56,17 +56,22 @@ function getManager() {
     .prompt([
       {
         type: "input",
-        message: "What is your employer ID?",
+        message: "What is the Manager's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is their employer ID?",
         name: "id",
       },
       {
         type: "input",
-        message: "What is your email?",
+        message: "What is their email?",
         name: "email",
       },
       {
         type: "input",
-        message: "What is your office number?",
+        message: "What is their office number?",
         name: "officeNumber",
       },
     ])
@@ -87,17 +92,22 @@ function getEngineer() {
     .prompt([
       {
         type: "input",
-        message: "What is your employer ID?",
+        message: "What is the Engineer's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is their employer ID?",
         name: "id",
       },
       {
         type: "input",
-        message: "What is your email?",
+        message: "What is their email?",
         name: "email",
       },
       {
         type: "input",
-        message: "What is your github username?",
+        message: "What is their github username?",
         name: "GitHub",
       },
     ])
@@ -118,17 +128,22 @@ function getIntern() {
     .prompt([
       {
         type: "input",
-        message: "What is your employer ID?",
+        message: "What is the intern name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is their employer ID?",
         name: "id",
       },
       {
         type: "input",
-        message: "What is your email?",
+        message: "What is their email?",
         name: "email",
       },
       {
         type: "input",
-        message: "What school are you attending?",
+        message: "What school are they attending?",
         name: "school",
       },
     ])
@@ -144,25 +159,3 @@ function renderHTML() {
 }
 
 newExecute();
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
